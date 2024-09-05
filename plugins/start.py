d5d62e54af16b1500be421f88bbe6da7b550baf8
Yuta-paradox <Animeparadox@yahoo.com>
2024-09-05 16:46:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "600"))
+SECONDS = int(os.getenv("SECONDS", "1800"))
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -103,8 +103,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('⚡️Ongoing Anime', url='https://t.me/Ongoing_anime_zone_x10'),
-                    InlineKeyboardButton('🍁Anime Channel', url='https://t.me/anime_zone_x10')
+                    InlineKeyboardButton('⚡️Backup channel', url='https://t.me/Paradox_backup_channel'),
+                    InlineKeyboardButton('🍁Movie Channel', url='https://t.me/+73YgnV-QBq04Njc9')
                 ]
             ]
         )
